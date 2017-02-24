1b0b3345b11b20a0463cfd82277b3e915d17c9f0
brightlights217 <wlac217@gmail.com>
2017-02-24 00:11:54 -0500

    Updated with comments

--- eloquentJS_chessBoard.js
+++ eloquentJS_chessBoard.js
@@ -1,13 +1,30 @@
-console.log("My chessboard below:");
+/*
+Eloquent Javascript - Exercise 2.3 - Chess board
 
-var variedChessboard = "";
+Write a program that creates a string that represents an 8×8 grid, using newline characters to separate lines. At each position of the grid there is either a space or a “#” character. The characters should form a chess board.
 
-var sizeOfChessboard = 8;
+Passing this string to console.log should show something like this:
 
-for ( var cols = 0; cols < sizeOfChessboard ; cols++ ) { //creates each row
-	var line = "";
+ # # # #
+# # # #
+ # # # #
+# # # #
+ # # # #
+# # # #
+ # # # #
+# # # #
+When you have a program that generates this pattern, define a variable size = 8 and change the program so that it works for any size, outputting a grid of the given width and height.
+*/
+
+var Chessboard = "";
+
+var sizeOfChessboard = 8; // change this integer to specify the chessboard size.  
+                          // this integer will be the same length and width 
+
+for ( var cols = 0; cols < sizeOfChessboard ; cols++ ) { // creates each row
+	var line = "";  // initialize the line
 	//  DEBUG console.log("column " + cols);
-	for ( var rows = 0 ; rows < sizeOfChessboard ; rows++ ) { //creates line
+	for ( var rows = 0 ; rows < sizeOfChessboard ; rows++ ) { // creates line
 		//  DEBUG console.log("row " + rows);
 		if ( (rows + cols) % 2 == 0 ) {
 			line += " ";
@@ -15,7 +32,7 @@ for ( var cols = 0; cols < sizeOfChessboard ; cols++ ) { //creates each row
 			line += "#";
 		}
 	};
-	variedChessboard += line + "\n";
+	Chessboard += line + "\n";
 };
 
-console.log(variedChessboard);
+console.log(Chessboard);
